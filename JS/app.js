3fc113c48fd6e2e4df4fb0e0d77bcedb446cbc3a
Cristhian Medrano Bonora <167735360+IceHeaven9@users.noreply.github.com>
2024-05-08 14:42:10 +0200

    Cristhian_JS_changes

--- JS/app.js
+++ JS/app.js
@@ -1,56 +1,82 @@
 "use strict";
+
 //Crear un div dentro del body llamado divUser
 const divUser = document.createElement("div");
 divUser.id = "divUser";
 document.body.appendChild(divUser);
+
+// Crear un div para el contador
+const divContador = document.createElement("div");
+divContador.id = "contador";
+divUser.appendChild(divContador);
+
 //Escribe un texto que ponga el valor del input user
 const user = document.getElementById("user");
 const h2 = document.createElement("h2");
 h2.textContent = user.value;
 divUser.appendChild(h2);
+
 //Quiero que en H2 se muestre el valor guardado del input user
 user.addEventListener("input", function () {
-  h2.textContent = "Suerte, " + user.value + "!";
+	h2.textContent = "Suerte, " + user.value + "!";
+});
+
+//Esta es la funcion que incia el contador
+const startGameCouter = document.getElementById("startGame");
+
+startGame.addEventListener("click", function () {
+	// Iniciar un contador de 10 segundos
+	let contador = 10;
+	const intervalId = setInterval(() => {
+		divContador.textContent = `Contador: ${contador}`;
+		contador--;
+
+		if (contador < 0) {
+			clearInterval(intervalId);
+			divContador.textContent = "Contador: 0";
+		}
+	}, 1000);
 });
 
 window.onload = function () {
-  // Obtén el contenedor donde quieres agregar las tarjetas
-  const container = document.querySelector(".container");
+	// Obtén el contenedor donde quieres agregar las tarjetas
+	const container = document.querySelector(".container");
 
-  // Define la cantidad de tarjetas que quieres crear
-  const numberOfCards = 16;
+	// Define la cantidad de tarjetas que quieres crear
+	const numberOfCards = 16;
 
-  // Crea las tarjetas
-  for (let i = 0; i < numberOfCards; i++) {
-    // Crea los elementos necesarios
-    const card = document.createElement("article");
-    const content = document.createElement("ul");
-    const front = document.createElement("li");
-    const back = document.createElement("li");
-    const img = document.createElement("img");
-    // Añade las clases necesarias
-    card.classList.add("card");
-    content.classList.add("content");
-    front.classList.add("front");
-    back.classList.add("back");
+	// Crea las tarjetas
+	for (let i = 0; i < numberOfCards; i++) {
+		// Crea los elementos necesarios
+		const card = document.createElement("article");
+		const content = document.createElement("ul");
+		const front = document.createElement("li");
+		const back = document.createElement("li");
+		const img = document.createElement("img");
 
-    // Añade el atributo src a la imagen
-    img.src = "/images/Logo_HAB_PNG.png";
-    img.alt = "logo hack a boss";
+		// Añade las clases necesarias
+		card.classList.add("card");
+		content.classList.add("content");
+		front.classList.add("front");
+		back.classList.add("back");
 
-    // Añade la imagen al frente de la tarjeta
-    front.appendChild(img);
+		// Añade el atributo src a la imagen
+		img.src = "/images/Logo_HAB_PNG.png";
+		img.alt = "logo hack a boss";
 
-    // Añade el frente y el dorso al contenido de la tarjeta
-    content.appendChild(front);
-    content.appendChild(back);
+		// Añade la imagen al frente de la tarjeta
+		front.appendChild(img);
 
-    // Añade el contenido a la tarjeta
-    card.appendChild(content);
+		// Añade el frente y el dorso al contenido de la tarjeta
+		content.appendChild(front);
+		content.appendChild(back);
 
-    // Añade la tarjeta al contenedor
-    container.appendChild(card);
-  }
+		// Añade el contenido a la tarjeta
+		card.appendChild(content);
+
+		// Añade la tarjeta al contenedor
+		container.appendChild(card);
+	}
 };
 
 // FUNCION PARA GIRAR LAS CARTAS AL HACER CLICK
@@ -58,14 +84,14 @@ window.onload = function () {
 const cards = document.querySelectorAll(".card");
 
 const reveal = (e) => {
-  const currentCard = e.currentTarget;
-  currentCard.classList.add("flipped");
+	const currentCard = e.currentTarget;
+	currentCard.classList.add("flipped");
 
-  setTimeout(() => {
-    currentCard.classList.remove("flipped");
-  }, 1000);
+	setTimeout(() => {
+		currentCard.classList.remove("flipped");
+	}, 1000);
 };
 
 for (const card of cards) {
-  card.addEventListener("click", reveal);
+	card.addEventListener("click", reveal);
 }
