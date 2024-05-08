6a733b451bc1aba6600c5ea43a546566d484a795
¨Marc Olle¨ <marc.olle01@gmail.com>
2024-05-08 11:57:33 +0200

    div-user-insert

--- JS/app.js
+++ JS/app.js
@@ -1,74 +1,54 @@
 "use strict";
-// crear un elemnto que contenga el nombre de usuario introducido en el input del form
-// añadir accion al boton de submit del form para iniciar un contador con el numero de intentos realizados
-// añadir un contador de intentos en el html
-// añadir un contador de aciertos en el html
-// añadir un boton de reiniciar el juego
-// añadir un boton de salir del juego
-
-document.querySelector("form").addEventListener("submit", (event) => {
-	// crear una variable y guardar el valor del input en el localStorage
-
-	event.preventDefault();
-	const userName = document.querySelector("#user").value;
-	if (userName.trim() === "") return alert("Por favor, introduce tu nombre.");
-	localStorage.setItem("userName", userName);
-
-	header.classList.remove("centered");
-	header.classList.add("moved-up");
-
-	document
-		.querySelectorAll(".card")
-		.forEach((card) =>
-			Object.assign(card.style, { visibility: "visible", opacity: 1 })
-		);
+//Crear un div dentro del body llamado divUser
+const divUser = document.createElement("div");
+divUser.id = "divUser";
+document.body.appendChild(divUser);
+//Escribe un texto que ponga el valor del input user
+const user = document.getElementById("user");
+const h2 = document.createElement("h2");
+h2.textContent = user.value;
+divUser.appendChild(h2);
+//Quiero que en H2 se muestre el valor guardado del input user
+user.addEventListener("input", function () {
+  h2.textContent = user.value;
 });
 
 window.onload = function () {
-	// Obtén el contenedor donde quieres agregar las tarjetas
-	const container = document.querySelector(".container");
+  // Obtén el contenedor donde quieres agregar las tarjetas
+  const container = document.querySelector(".container");
 
-	// Define la cantidad de tarjetas que quieres crear
-	const numberOfCards = 16;
+  // Define la cantidad de tarjetas que quieres crear
+  const numberOfCards = 16;
 
-	// Crea las tarjetas
-	for (let i = 0; i < numberOfCards; i++) {
-		// Crea los elementos necesarios
-		const card = document.createElement("article");
-		const content = document.createElement("ul");
-		const front = document.createElement("li");
-		const back = document.createElement("li");
-		const img = document.createElement("img");
-		// Añade las clases necesarias
-		card.classList.add("card");
-		content.classList.add("content");
-		front.classList.add("front");
-		back.classList.add("back");
+  // Crea las tarjetas
+  for (let i = 0; i < numberOfCards; i++) {
+    // Crea los elementos necesarios
+    const card = document.createElement("article");
+    const content = document.createElement("ul");
+    const front = document.createElement("li");
+    const back = document.createElement("li");
+    const img = document.createElement("img");
+    // Añade las clases necesarias
+    card.classList.add("card");
+    content.classList.add("content");
+    front.classList.add("front");
+    back.classList.add("back");
 
-		// Añade el atributo src a la imagen
-		img.src = "/images/Logo_HAB_PNG.png";
-		img.alt = "logo hack a boss";
+    // Añade el atributo src a la imagen
+    img.src = "/images/Logo_HAB_PNG.png";
+    img.alt = "logo hack a boss";
 
-		// Añade la imagen al frente de la tarjeta
-		front.appendChild(img);
+    // Añade la imagen al frente de la tarjeta
+    front.appendChild(img);
 
-		// Añade el frente y el dorso al contenido de la tarjeta
-		content.appendChild(front);
-		content.appendChild(back);
+    // Añade el frente y el dorso al contenido de la tarjeta
+    content.appendChild(front);
+    content.appendChild(back);
 
-		// Añade el contenido a la tarjeta
-		card.appendChild(content);
+    // Añade el contenido a la tarjeta
+    card.appendChild(content);
 
-		// Añade la tarjeta al contenedor
-		container.appendChild(card);
-	}
-	const mainContent = document.querySelector(".mainContent");
-	// crear una variable que contenga el nombre de usuario introducido en el input del form
-
-	const userDiv = document.createElement("div");
-	const userName = document.createElement("p");
-	const localStorageResult = localStorage.getItem("userName");
-	userName.textContent = `Nombre de usuario: ${localStorageResult}`; // Reemplaza esto con el nombre de usuario real
-	userDiv.appendChild(userName);
-	mainContent.appendChild(userDiv);
+    // Añade la tarjeta al contenedor
+    container.appendChild(card);
+  }
 };
--- JS/visibilityControls.js
+++ JS/visibilityControls.js
@@ -8,15 +8,17 @@ document.addEventListener("DOMContentLoaded", function () {
   const headerContent = document.querySelector("header");
   const startGameBtn = document.getElementById("startGame");
   const userInput = document.getElementById("user");
+  const divprueba = document.querySelector("div");
 
   // Inicialmente, oculta solo el contenido principal del juego
   gameContent.classList.add("hidden");
-
+  divprueba.classList.add("hidden");
   startGameBtn.addEventListener("click", function (event) {
     event.preventDefault(); // Previene el comportamiento por defecto del formulario
     if (userInput.value.trim() !== "") {
       gameContent.classList.remove("hidden"); // Muestra el contenido del juego
       headerContent.classList.add("hidden"); // Oculta el header
+      divprueba.classList.remove("hidden");
     } else {
       alert("Por favor, ingresa tu nombre para comenzar el juego.");
     }
