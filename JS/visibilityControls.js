17daff3786f7283c6215813f6b02c7eb71baf46f
Iván Sánchez <mourazos1995@gmail.com>
2024-05-08 21:10:08 +0200

    changes-and-adds

--- JS/visibilityControls.js
+++ JS/visibilityControls.js
@@ -4,30 +4,33 @@
 //
 
 document.addEventListener("DOMContentLoaded", function () {
-	const gameContent = document.getElementById("gameContent");
-	const headerContent = document.querySelector("header");
-	const startGameBtn = document.getElementById("startGame");
-	const userInput = document.getElementById("user");
-	const divUserContent = document.getElementById("divUser");
-	const divContadorContent = document.getElementById("divContador");
-	const divTextContadorContent = document.getElementById("divTextContador");
+  const gameContent = document.getElementById("gameContent");
+  const headerContent = document.querySelector("header");
+  const startGameBtn = document.getElementById("startGame");
+  const userInput = document.getElementById("user");
+  const divUserContent = document.getElementById("divUser");
+  const divContadorContent = document.getElementById("divContador");
+  const divTextContadorContent = document.getElementById("divTextContador");
+  const footerContent = document.querySelector("footer");
 
-	// Inicialmente, oculta solo el contenido principal del juego
-	divUserContent.classList.add("hidden");
-	gameContent.classList.add("hidden");
-	divContadorContent.classList.add("hidden");
-	divTextContadorContent.classList.add("hidden");
+  // Inicialmente, oculta solo el contenido principal del juego
+  divUserContent.classList.add("hidden");
+  gameContent.classList.add("hidden");
+  divContadorContent.classList.add("hidden");
+  divTextContadorContent.classList.add("hidden");
+  footerContent.classList.add("hidden");
 
-	startGameBtn.addEventListener("click", function (event) {
-		event.preventDefault();
-		if (userInput.value.trim() !== "") {
-			divUserContent.classList.remove("hidden");
-			gameContent.classList.remove("hidden");
-			headerContent.classList.add("hidden");
-			divContadorContent.classList.remove("hidden");
-			divTextContadorContent.classList.remove("hidden");
-		} else {
-			alert("Por favor, ingresa tu nombre para comenzar el juego.");
-		}
-	});
+  startGameBtn.addEventListener("click", function (event) {
+    event.preventDefault();
+    if (userInput.value.trim() !== "") {
+      divUserContent.classList.remove("hidden");
+      gameContent.classList.remove("hidden");
+      headerContent.classList.add("hidden");
+      divContadorContent.classList.remove("hidden");
+      divTextContadorContent.classList.remove("hidden");
+      footerContent.classList.remove("hidden");
+    } else {
+      alert("Por favor, ingresa tu nombre para comenzar el juego.");
+    }
+  });
 });
