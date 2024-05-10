69c906edbbd1379364eb328344ffc6d1c27be74f
Cristhian Medrano Bonora <167735360+IceHeaven9@users.noreply.github.com>
2024-05-10 03:39:25 +0200

    Cristhian_JS_CSS_changes

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,11 @@
 "use strict";
 
+const mainElements = document.querySelector("main");
+
 //Crear un div dentro del body llamado divUser
 const divUser = document.createElement("div");
 divUser.id = "divUser";
-document.body.appendChild(divUser);
+mainElements.appendChild(divUser);
 
 //Escribe un texto que ponga el valor del input user
 const user = document.getElementById("user");
@@ -13,7 +15,7 @@ divUser.appendChild(h2);
 
 // Crear un div para el contador
 const divContador = document.createElement("div");
-document.body.appendChild(divContador);
+mainElements.appendChild(divContador);
 divContador.id = "divContador";
 
 // Crear un h3 para el contador
@@ -22,23 +24,35 @@ h3Contador.classList.add("contador");
 h3Contador.textContent = "";
 divContador.appendChild(h3Contador);
 
-//Quiero que en H2 se muestre el valor del input user y siempre con la primera letra en mayúscula
-user.addEventListener("input", function () {
-  h2.textContent =
-    "Suerte, " +
-    user.value
-      .split(" ")
-      .map(function (word) {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-      })
-      .join(" ") +
-    "!";
-});
-
 // Un div para el texto del contador
 const divTextContador = document.createElement("div");
 divTextContador.id = "divTextContador";
-document.body.appendChild(divTextContador);
+mainElements.appendChild(divTextContador);
+
+// Crear un div para el contador de intentos
+const divIntentos = document.createElement("div");
+divIntentos.id = "divIntentos";
+divIntentos.textContent = "Intentos: 0";
+mainElements.appendChild(divIntentos);
+
+// Crear un div para el contador de intentos fallidos
+const divContadorDeFallos = document.createElement("div");
+divContadorDeFallos.id = "divFallos";
+divContadorDeFallos.textContent = "Fallos: 0";
+mainElements.appendChild(divContadorDeFallos);
+
+//Quiero que en H2 se muestre el valor del input user y siempre con la primera letra en mayúscula
+user.addEventListener("input", function () {
+	h2.textContent =
+		"Suerte, " +
+		user.value
+			.split(" ")
+			.map(function (word) {
+				return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+			})
+			.join(" ") +
+		"!";
+});
 
 //
 // FUNCION PARA ACTIVAR CONTADOR Y OCULTARLO AL FINALIZAR
@@ -47,74 +61,74 @@ document.body.appendChild(divTextContador);
 // Iniciar un contador de 10 segundos
 let contador;
 startGame.addEventListener("click", function () {
-  contador = 10;
-  let tiempoInicio = Date.now();
-  let tiempoFinal = tiempoInicio + contador * 1000;
-  const intervalId = setInterval(() => {
-    let tiempoRestante = tiempoFinal - Date.now();
-    contador = Math.round(tiempoRestante / 1000);
-    divTextContador.textContent = `Tiempo restante: ${contador} seg`;
-    contador--;
+	contador = 10;
+	let tiempoInicio = Date.now();
+	let tiempoFinal = tiempoInicio + contador * 1000;
+	const intervalId = setInterval(() => {
+		let tiempoRestante = tiempoFinal - Date.now();
+		contador = Math.round(tiempoRestante / 1000);
+		divTextContador.textContent = `Tiempo restante: ${contador} seg`;
+		contador--;
 
-    if (contador < 0) {
-      clearInterval(intervalId);
-      h3Contador.textContent = "";
-      divTextContador.textContent = "";
-      divTextContador.classList.add("hidden");
-    }
-  }, 10);
+		if (contador < 0) {
+			clearInterval(intervalId);
+			h3Contador.textContent = "";
+			divTextContador.textContent = "";
+			divTextContador.classList.add("hidden");
+
+			const flippedCards = document.querySelectorAll(".card.flipped");
+
+			for (const card of flippedCards) {
+				card.classList.remove("flipped");
+			}
+		}
+	}, 100);
 });
-
-// Crear un div para el contador de intentos
-const divIntentos = document.createElement("div");
-divIntentos.id = "divIntentos";
-divIntentos.textContent = "Intentos: 0";
-document.body.appendChild(divIntentos);
 
 //
 // FUNCION PARA CREAR LAS TARJETAS
 //
 
 window.onload = function () {
-  // Restablecer el valor del input "user" a null
-  const user = document.getElementById("user");
-  user.value = null;
-  // Obtén el contenedor donde quieres agregar las tarjetas
-  const container = document.querySelector(".container");
+	// Restablecer el valor del input "user" a null
+	const user = document.getElementById("user");
+	user.value = null;
+	// Obtén el contenedor donde quieres agregar las tarjetas
+	const container = document.querySelector(".container");
 
-  // Define la cantidad de tarjetas que quieres crear
-  const numberOfCards = 16;
+	// Define la cantidad de tarjetas que quieres crear
+	const numberOfCards = 16;
 
-  // Crea las tarjetas
-  for (let i = 0; i < numberOfCards; i++) {
-    // Crea los elementos necesarios
-    const card = document.createElement("article");
-    const content = document.createElement("ul");
-    const front = document.createElement("li");
-    const back = document.createElement("li");
-    const img = document.createElement("img");
+	// Crea las tarjetas
+	for (let i = 0; i < numberOfCards; i++) {
+		// Crea los elementos necesarios
+		const card = document.createElement("article");
+		const content = document.createElement("ul");
+		const front = document.createElement("li");
+		const back = document.createElement("li");
+		const img = document.createElement("img");
 
-    // Añade las clases necesarias
-    card.classList.add("card");
-    content.classList.add("content");
-    front.classList.add("front");
-    back.classList.add("back");
+		// Añade las clases necesarias
+		card.classList.add("card");
+		content.classList.add("content");
+		front.classList.add("front");
+		back.classList.add("back");
 
-    // Añade el atributo src a la imagen
-    img.src = "/images/Logo_HAB_PNG.png";
-    img.alt = "logo hack a boss";
+		// Añade el atributo src a la imagen
+		img.src = "/images/Logo_HAB_PNG.png";
+		img.alt = "logo hack a boss";
 
-    // Añade la imagen al frente de la tarjeta
-    front.appendChild(img);
+		// Añade la imagen al frente de la tarjeta
+		front.appendChild(img);
 
-    // Añade el frente y el dorso al contenido de la tarjeta
-    content.appendChild(front);
-    content.appendChild(back);
+		// Añade el frente y el dorso al contenido de la tarjeta
+		content.appendChild(front);
+		content.appendChild(back);
 
-    // Añade el contenido a la tarjeta
-    card.appendChild(content);
+		// Añade el contenido a la tarjeta
+		card.appendChild(content);
 
-    // Añade la tarjeta al contenedor
-    container.appendChild(card);
-  }
+		// Añade la tarjeta al contenedor
+		container.appendChild(card);
+	}
 };
--- JS/visibilityControls.js
+++ JS/visibilityControls.js
@@ -1,131 +1,147 @@
 "use strict";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const gameContent = document.getElementById("gameContent");
-  const headerContent = document.querySelector("header");
-  const startGameBtn = document.getElementById("startGame");
-  const userInput = document.getElementById("user");
-  const container = document.querySelector(".container");
-  const divUserContent = document.getElementById("divUser");
-  const divContadorContent = document.getElementById("divContador");
-  const divTextContadorContent = document.getElementById("divTextContador");
+	const gameContent = document.getElementById("gameContent");
+	const headerContent = document.querySelector("header");
+	const startGameBtn = document.getElementById("startGame");
+	const userInput = document.getElementById("user");
+	const container = document.querySelector(".container");
+	const divUserContent = document.getElementById("divUser");
+	const divContadorContent = document.getElementById("divContador");
+	const divTextContadorContent = document.getElementById("divTextContador");
+	const divIntentosContent = document.getElementById("divIntentos");
 
-  gameContent.classList.add("hidden");
-  divUserContent.classList.add("hidden");
-  divContadorContent.classList.add("hidden");
-  divTextContadorContent.classList.add("hidden");
+	gameContent.classList.add("hidden");
+	divUserContent.classList.add("hidden");
+	divContadorContent.classList.add("hidden");
+	divTextContadorContent.classList.add("hidden");
+	divIntentosContent.classList.add("hidden");
 
-  startGameBtn.addEventListener("click", function (event) {
-    event.preventDefault();
-    if (userInput.value.trim() !== "") {
-      headerContent.classList.add("hidden");
-      gameContent.classList.remove("hidden");
-      divUserContent.classList.remove("hidden");
-      divContadorContent.classList.remove("hidden");
-      divTextContadorContent.classList.remove("hidden");
+	startGameBtn.addEventListener("click", function (event) {
+		event.preventDefault();
+		if (userInput.value.trim() !== "") {
+			headerContent.classList.add("hidden");
+			gameContent.classList.remove("hidden");
+			divUserContent.classList.remove("hidden");
+			divContadorContent.classList.remove("hidden");
+			divTextContadorContent.classList.remove("hidden");
+			divIntentosContent.classList.remove("hidden");
 
-      let cardBackImage = [
-        { src: "/images/1.png", value: 1 },
-        { src: "/images/2.png", value: 2 },
-        { src: "/images/3.png", value: 3 },
-        { src: "/images/4.png", value: 4 },
-        { src: "/images/5.png", value: 5 },
-        { src: "/images/6.png", value: 6 },
-        { src: "/images/7.png", value: 7 },
-        { src: "/images/8.png", value: 8 },
-        { src: "/images/1c.png", value: 1 },
-        { src: "/images/2c.png", value: 2 },
-        { src: "/images/3c.png", value: 3 },
-        { src: "/images/4c.png", value: 4 },
-        { src: "/images/5c.png", value: 5 },
-        { src: "/images/6c.png", value: 6 },
-        { src: "/images/7c.png", value: 7 },
-        { src: "/images/8c.png", value: 8 },
-      ];
+			let cardBackImage = [
+				{ src: "/images/1.png", value: 1 },
+				{ src: "/images/2.png", value: 2 },
+				{ src: "/images/3.png", value: 3 },
+				{ src: "/images/4.png", value: 4 },
+				{ src: "/images/5.png", value: 5 },
+				{ src: "/images/6.png", value: 6 },
+				{ src: "/images/7.png", value: 7 },
+				{ src: "/images/8.png", value: 8 },
+			];
 
-      // Mezclar las tarjetas
-      cardBackImage = cardBackImage.sort(() => Math.random() - 0.5);
+			cardBackImage = cardBackImage.concat(cardBackImage);
+			// Mezclar las tarjetas
+			cardBackImage = cardBackImage.sort(() => Math.random() - 0.5);
 
-      // Limpiar el contenedor de tarjetas antes de agregar nuevas tarjetas
-      container.innerHTML = "";
+			// Limpiar el contenedor de tarjetas antes de agregar nuevas tarjetas
+			container.innerHTML = "";
 
-      // Crear las tarjetas
-      for (let i = 0; i < cardBackImage.length; i++) {
-        const card = document.createElement("article");
-        const content = document.createElement("ul");
-        const front = document.createElement("li");
-        const back = document.createElement("li");
-        const img = document.createElement("img");
+			// Crear las tarjetas
+			for (let i = 0; i < cardBackImage.length; i++) {
+				const card = document.createElement("article");
+				const content = document.createElement("ul");
+				const front = document.createElement("li");
+				const back = document.createElement("li");
+				const img = document.createElement("img");
 
-        card.classList.add("card");
-        content.classList.add("content");
-        front.classList.add("front");
-        back.classList.add("back");
+				card.classList.add("card");
+				card.classList.add("flipped");
+				content.classList.add("content");
+				front.classList.add("front");
+				back.classList.add("back");
 
-        // Asignar valor a la tarjeta trasera
-        img.src = cardBackImage[i].src;
-        img.dataset.value = cardBackImage[i].value;
-        back.dataset.value = cardBackImage[i].value;
-        back.appendChild(img);
+				// Asignar valor a la tarjeta trasera
+				img.src = cardBackImage[i].src;
+				img.dataset.value = cardBackImage[i].value;
+				back.dataset.value = cardBackImage[i].value;
+				back.appendChild(img);
 
-        // Asignar imagen al frente
-        const frontImg = document.createElement("img");
-        frontImg.src = "/images/Logo_HAB_PNG.png";
-        frontImg.alt = "logo hack a boss";
-        front.appendChild(frontImg);
+				// Asignar imagen al frente
+				const frontImg = document.createElement("img");
+				frontImg.src = "/images/Logo_HAB_PNG.png";
+				frontImg.alt = "logo hack a boss";
+				front.appendChild(frontImg);
 
-        content.appendChild(front);
-        content.appendChild(back);
-        card.appendChild(content);
-        container.appendChild(card);
+				content.appendChild(front);
+				content.appendChild(back);
+				card.appendChild(content);
+				container.appendChild(card);
 
-        card.addEventListener("click", reveal);
-      }
+				card.addEventListener("click", reveal);
+			}
 
-      let flippedCards = [];
+			let flippedCards = [];
 
-      // Función para girar las tarjetas
-      function reveal(e) {
-        const currentCard = e.currentTarget;
-        if (
-          flippedCards.length < 2 &&
-          !currentCard.classList.contains("flipped")
-        ) {
-          currentCard.classList.add("flipped");
-          flippedCards.push(currentCard);
-        }
+			// Función para girar las tarjetas
+			function reveal(e) {
+				const currentCard = e.currentTarget;
+				if (
+					flippedCards.length < 2 &&
+					!currentCard.classList.contains("flipped")
+				) {
+					currentCard.classList.add("flipped");
+					flippedCards.push(currentCard);
+				}
 
-        // Si hay dos tarjetas volteadas
-        if (flippedCards.length === 2) {
-          const img1Value =
-            flippedCards[0].querySelector(".back img").dataset.value;
-          const img2Value =
-            flippedCards[1].querySelector(".back img").dataset.value;
+				// Si hay dos tarjetas volteadas
+				if (flippedCards.length === 2) {
+					const img1Value =
+						flippedCards[0].querySelector(".back img").dataset.value;
+					const img2Value =
+						flippedCards[1].querySelector(".back img").dataset.value;
 
-          console.log(img1Value, img2Value);
+					console.log(img1Value, img2Value);
 
-          if (img1Value === img2Value) {
-            flippedCards = [];
-            incrementarIntentos();
-          } else {
-            setTimeout(() => {
-              flippedCards[0].classList.remove("flipped");
-              flippedCards[1].classList.remove("flipped");
-              flippedCards = [];
-              incrementarIntentos();
-            }, 500);
-          }
-        }
-      }
-    } else {
-      alert("Por favor, ingresa tu nombre para comenzar el juego.");
-    }
-  });
+					if (img1Value === img2Value) {
+						// Añadir la clase 'matched' a los elementos 'li' con la clase 'back'
+						flippedCards[0]
+							.querySelector(".back")
+							.classList.add("shadow-inset-center");
+						flippedCards[1]
+							.querySelector(".back")
+							.classList.add("shadow-inset-center");
+						flippedCards = [];
+						counters.incrementarIntentos();
+					} else {
+						setTimeout(() => {
+							flippedCards[0].classList.remove("flipped");
+							flippedCards[1].classList.remove("flipped");
+							flippedCards = [];
+							counters.incrementarIntentos();
+							counters.incrementarFallos();
+						}, 500);
+					}
+				}
+			}
+		} else {
+			alert("Por favor, ingresa tu nombre para comenzar el juego.");
+		}
+	});
 });
 
-// Crear una funcion que incremente el contador de intentos y que sume uno cada vez que se haga click en dos tarjetas
-let intentos = 0;
-function incrementarIntentos() {
-  intentos++;
-  divIntentos.textContent = `Intentos: ${intentos}`;
-}
+// Create an object to manage counters
+let counters = {
+	intentos: 0,
+	fallos: 0,
+
+	// Function to increment intentos counter
+	incrementarIntentos: function () {
+		this.intentos++;
+		divIntentos.textContent = `Intentos: ${this.intentos}`;
+	},
+
+	// Function to increment fallos counter
+	incrementarFallos: function () {
+		this.fallos++;
+		divFallos.textContent = `Fallos: ${this.fallos}`;
+	},
+};
