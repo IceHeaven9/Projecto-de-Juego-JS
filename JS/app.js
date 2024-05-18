6e6fadefed1131203ff893c5b8840c20505dadb0
Cristhian Medrano Bonora <167735360+IceHeaven9@users.noreply.github.com>
2024-05-18 12:07:09 +0200

    cristhian_deploy_FIX

--- JS/app.js
+++ JS/app.js
@@ -10,11 +10,11 @@ mainElements.appendChild(divUser);
 //Texto que pone el valor del input a user
 const user = document.getElementById("user");
 function handleInputChange() {
-  // Recupera el valor del input
-  const userName = user.value;
+	// Recupera el valor del input
+	const userName = user.value;
 
-  // Guarda el valor en localStorage
-  localStorage.setItem("user", userName);
+	// Guarda el valor en localStorage
+	localStorage.setItem("user", userName);
 }
 
 // Añade el controlador de eventos al input
@@ -79,46 +79,46 @@ btnRanking.textContent = "Ver ranking";
 divRanking.appendChild(btnRanking);
 
 const recuperarValoresYActualizarRanking = () => {
-  // Recupera los valores del localStorage
-  const user = localStorage.getItem("user");
-  const nota = parseFloat(localStorage.getItem("nota"));
+	// Recupera los valores del localStorage
+	const user = localStorage.getItem("user");
+	const nota = parseFloat(localStorage.getItem("nota"));
 
-  // Recupera el array del localStorage o crea uno nuevo si no existe
-  let arrayUsuarioNota =
-    JSON.parse(localStorage.getItem("arrayUsuarioNota")) || [];
+	// Recupera el array del localStorage o crea uno nuevo si no existe
+	let arrayUsuarioNota =
+		JSON.parse(localStorage.getItem("arrayUsuarioNota")) || [];
 
-  // Busca si el usuario ya existe en el array
-  const usuarioExistente = arrayUsuarioNota.find(
-    (elemento) => elemento.user === user
-  );
+	// Busca si el usuario ya existe en el array
+	const usuarioExistente = arrayUsuarioNota.find(
+		(elemento) => elemento.user === user
+	);
 
-  if (usuarioExistente) {
-    // Si el usuario existe y la nueva nota es mayor que la existente, actualiza la nota
-    if (nota > usuarioExistente.nota) {
-      usuarioExistente.nota = nota;
-    }
-  } else {
-    // Si el usuario no existe, añade el nuevo usuario y nota al array
-    // Si el array ya tiene 5 elementos, elimina el primer elemento
-    if (arrayUsuarioNota.length === 5) {
-      arrayUsuarioNota.shift();
-    }
-    arrayUsuarioNota.push({ user, nota });
-  }
+	if (usuarioExistente) {
+		// Si el usuario existe y la nueva nota es mayor que la existente, actualiza la nota
+		if (nota > usuarioExistente.nota) {
+			usuarioExistente.nota = nota;
+		}
+	} else {
+		// Si el usuario no existe, añade el nuevo usuario y nota al array
+		// Si el array ya tiene 5 elementos, elimina el primer elemento
+		if (arrayUsuarioNota.length === 5) {
+			arrayUsuarioNota.shift();
+		}
+		arrayUsuarioNota.push({ user, nota });
+	}
 
-  // Guarda el array en el localStorage
-  localStorage.setItem("arrayUsuarioNota", JSON.stringify(arrayUsuarioNota));
+	// Guarda el array en el localStorage
+	localStorage.setItem("arrayUsuarioNota", JSON.stringify(arrayUsuarioNota));
 
-  // Ordena el array por la nota de mayor a menor
-  arrayUsuarioNota.sort((a, b) => b.nota - a.nota);
+	// Ordena el array por la nota de mayor a menor
+	arrayUsuarioNota.sort((a, b) => b.nota - a.nota);
 
-  // Recorre el array y añade un nuevo elemento "p" con el valor de "user" y "nota" de cada objeto del array al "divRanking"
-  arrayUsuarioNota.forEach((elemento) => {
-    const p = document.createElement("p");
-    p.classList.add("pRanking");
-    p.textContent = `${elemento.user}: ${elemento.nota}`;
-    divRanking.appendChild(p);
-  });
+	// Recorre el array y añade un nuevo elemento "p" con el valor de "user" y "nota" de cada objeto del array al "divRanking"
+	arrayUsuarioNota.forEach((elemento) => {
+		const p = document.createElement("p");
+		p.classList.add("pRanking");
+		p.textContent = `${elemento.user}: ${elemento.nota}`;
+		divRanking.appendChild(p);
+	});
 };
 
 // Llama a la función para recuperar los valores y actualizar el ranking
@@ -138,21 +138,21 @@ divResultado.appendChild(btnExitGame);
 
 //Funcion para que H2 se muestre el valor del input user y siempre con la primera letra en mayúscula
 user.addEventListener("input", function () {
-  const maxLength = 15;
-  const userValue = user.value;
-  if (userValue.length > maxLength) {
-    user.value = userValue.slice(0, maxLength);
-  }
+	const maxLength = 15;
+	const userValue = user.value;
+	if (userValue.length > maxLength) {
+		user.value = userValue.slice(0, maxLength);
+	}
 
-  h2.textContent =
-    "Suerte, " +
-    user.value
-      .split(" ")
-      .map(function (word) {
-        return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
-      })
-      .join(" ") +
-    "!";
+	h2.textContent =
+		"Suerte, " +
+		user.value
+			.split(" ")
+			.map(function (word) {
+				return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+			})
+			.join(" ") +
+		"!";
 });
 
 // Funcion para activar contador y ocultarlo al finalizar
@@ -161,48 +161,48 @@ let contador;
 let intervalId;
 
 function startTimer() {
-  contador = 10;
-  let tiempoInicio = Date.now();
-  let tiempoFinal = tiempoInicio + contador * 1000;
-  intervalId = setInterval(() => {
-    let tiempoRestante = tiempoFinal - Date.now();
-    contador = Math.round(tiempoRestante / 1000);
-    divTextContador.textContent = `Tiempo restante: ${contador} seg`;
-    contador--;
+	contador = 10;
+	let tiempoInicio = Date.now();
+	let tiempoFinal = tiempoInicio + contador * 1000;
+	intervalId = setInterval(() => {
+		let tiempoRestante = tiempoFinal - Date.now();
+		contador = Math.round(tiempoRestante / 1000);
+		divTextContador.textContent = `Tiempo restante: ${contador} seg`;
+		contador--;
 
-    const tarjetas = document.querySelectorAll(".card");
-    tarjetas.forEach((card) => {
-      card.classList.add("flipped");
-    });
+		const tarjetas = document.querySelectorAll(".card");
+		tarjetas.forEach((card) => {
+			card.classList.add("flipped");
+		});
 
-    const matchedCards = document.querySelectorAll(".back.matched");
-    matchedCards.forEach((card) => {
-      card.classList.remove("matched");
-    });
+		const matchedCards = document.querySelectorAll(".back.matched");
+		matchedCards.forEach((card) => {
+			card.classList.remove("matched");
+		});
 
-    if (contador < 0) {
-      clearInterval(intervalId);
+		if (contador < 0) {
+			clearInterval(intervalId);
 
-      divTextContador.textContent = "";
-      divTextContador.textContent = "Comienza el juego!!";
+			divTextContador.textContent = "";
+			divTextContador.textContent = "Comienza el juego!!";
 
-      const flippedCards = document.querySelectorAll(".card.flipped");
+			const flippedCards = document.querySelectorAll(".card.flipped");
 
-      for (const card of flippedCards) {
-        card.classList.remove("flipped");
-      }
-    }
-  }, 100);
+			for (const card of flippedCards) {
+				card.classList.remove("flipped");
+			}
+		}
+	}, 100);
 }
 
 startGame.addEventListener("click", startTimer);
 
 function resetGame() {
-  // Detener el temporizador actual
-  clearInterval(intervalId);
+	// Detener el temporizador actual
+	clearInterval(intervalId);
 
-  // Reiniciar el temporizador
-  startTimer();
+	// Reiniciar el temporizador
+	startTimer();
 }
 const resetBtnController = document.getElementById("resetBtn");
 // Añadir el controlador de eventos al botón de reset
@@ -210,51 +210,7 @@ resetBtnController.addEventListener("click", resetGame);
 const btnGameAgainContent = document.getElementById("btnGameAgain");
 btnGameAgainContent.addEventListener("click", resetGame);
 
-// Funcion para crear las tarjetas
-window.onload = function () {
-  // Restablecer el valor del input "user" a null
-  const user = document.getElementById("user");
-  user.value = null;
-  // Obtiene el contenedor donde queremos agregar las tarjetas
-  const container = document.querySelector(".container");
-
-  // Cantidad de tarjetas que quieres crear
-  const numberOfCards = 16;
-
-  // Crea las tarjetas
-  for (let i = 0; i < numberOfCards; i++) {
-    // Crea los elementos necesarios
-    const card = document.createElement("article");
-    const content = document.createElement("ul");
-    const front = document.createElement("li");
-    const back = document.createElement("li");
-    const img = document.createElement("img");
-
-    // Añade las clases necesarias
-    card.classList.add("card");
-    content.classList.add("content");
-    front.classList.add("front");
-    back.classList.add("back");
-
-    // Añade el atributo src a la imagen
-    img.src = "../images/Logo-hack-a-boss.png";
-    img.alt = "logo hack a boss";
-
-    // Añade la imagen al frente de la tarjeta
-    front.appendChild(img);
-
-    // Añade el frente y el dorso al contenido de la tarjeta
-    content.appendChild(front);
-    content.appendChild(back);
-
-    // Añade el contenido a la tarjeta
-    card.appendChild(content);
-
-    // Añade la tarjeta al contenedor
-    container.appendChild(card);
-  }
-};
-
+//
 //  IceHeaven, Ivan Sanchez, Marc Ollé, Santiago Cadavid..
 //         ─────▄██▀▀▀▀▀▀▀▀▀▀▀▀▀██▄─────
 //         ────███───────────────███────
